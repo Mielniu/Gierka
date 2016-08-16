9c18a169007a0a247bf95841b21185b8b5b80373
McMichael <McMichael@KOMPUTER>
2016-08-16 03:07:03 +0200

    New for

--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -5,5 +5,12 @@ int main()
 	std::cout << "Hello Team's" << std::endl;
 	int a;
 	std::cin >> a;
+	for (int i = 1; i <= 10; i++)
+	{
+		a += a;
+		std::cout << i << ": " << a << std::endl;
+	}
+	int b;
+	std::cin >> b;
 	return 0;
 }
